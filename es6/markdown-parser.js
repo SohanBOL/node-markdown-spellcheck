7ab53bdbafc3e54ddc8d10d2abb53d46b9601809
SohanBOL <66009833+SohanBOL@users.noreply.github.com>
2020-10-22 00:02:50 +0530

    es6: remove snake_case words

--- es6/markdown-parser.js
+++ es6/markdown-parser.js
@@ -16,6 +16,9 @@ export default function(src) {
     tracker.replaceAll(jekyllFrontMatter, " ");
   }
 
+  tracker.removeAll(/\[[^`]*\]/); // remove content in square bracket []
+  tracker.removeAll(/[a-zA-Z]+(_[a-zA-Z]*)+/) // remove snake_case words
+
   tracker.removeAll(/```[\w\W]*?```/);
   tracker.removeAll(/~~~[\w\W]*?~~~/);
   tracker.removeAll(/``[\w\W]*?``/);
